926932e848e3c42ec127b878bf3a6226fe6c67f7
Robin Roth <robin.roth1@students.fhnw.ch>
2026-04-25 17:22:36 +0200

    refactor functions to separate library file

--- install_sway.sh
+++ install_sway.sh
@@ -3,55 +3,23 @@
 # Install all required packages for sway and symlink the dotfiles.
 set -euo pipefail
 
+. lib/utils.sh
+
 SWAY_CONF_DIR="$HOME/.config/sway"
 SWAY_RUN_DIR="/usr/local/bin"
 KANSHI_CONF_DIR="$HOME/.config/kanshi"
 SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
 
-# get the systemd unit's current state 
-get_unit_state () {
-    local unit_state=$(systemctl --user list-unit-files "$1" \
-        --output json --no-pager | jq '.[].state') 
-
-    if [[ $unit_state == "" ]]; then
-        echo "missing"
-    else
-        echo "$unit_state" 
-    fi
-}
-
-# execute necessary commands to initialise systemd unit based on state
-init_unit () { 
-    local state=$(get_unit_state $1) 
-    local unit_file_repo="$2/$1"
-    local unit_type=${1##*.}
-    echo "State of $1: $state"
-    # don't enable target units
-    case "$state" in
-        missing) ln -fvs $unit_file_repo $3;&
-        disabled)
-            if [[ "${unit_type}" != target ]]; then
-                systemctl --user enable $1
-            fi     
-            ;;
-        *)
-            echo "Systemd unit $1 is already installed. Doing nothing."
-            return;;
-    esac
-}
 
 echo "Running sway install script ..."
 
-# update package information 
-sudo apt-get -qq update
-
 # install display manager and greeter
 printf "Installing display manager and greeter:\n"
-sudo apt-get -qy install greetd tuigreet
+apt_install greetd tuigreet
 
 # install required packages for config to work
 printf "\nInstalling sway and used dependencies:\n"
-sudo apt-get -qy install sway swayidle \
+apt_install sway swayidle \
     waybar \
     wlsunset \
     kitty \
